bb3ec8bfa79572bf32324b510ac2d1a5092f6810
GabinS <gabz98.gs@gmail.com>
2019-12-08 00:10:20 +0100

    Add top menu template

---
+++ src/components/ButtonHome.vue
@@ -0,0 +1,13 @@
+<template>
+    <router-link :to="{name: 'Home'}">
+        <div class="btn-home">
+            <i class="icon fa fa-home"></i>
+        </div>
+    </router-link>
+</template>
+
+<script>
+export default {
+    name: 'ButtonHome'
+}
+</script>
---
+++ src/components/VroomService/TopMenu.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="menu row">
+        <button-home class="col-md-2" />
+        <div class="items col-md-8">
+            <router-link :to="{name: ''}" class="item">Voitures</router-link>
+            <router-link :to="{name: ''}" class="item">Mes réservations</router-link>
+            <router-link :to="{name: ''}" class="item">Mon compte</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+import ButtonHome from '@/components/ButtonHome'
+
+export default {
+    name: 'TopMenu',
+    components: { ButtonHome }
+}
+</script>

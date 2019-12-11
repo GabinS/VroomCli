9b587d85b7da7723df2e0463b4e14de141e533d7
GabinS <gabz98.gs@gmail.com>
2019-12-11 10:22:01 +0100

    Fix axios call + change reservation page structure

--- src/components/VroomService/Reservations.vue
+++ src/components/VroomService/Reservations.vue
@@ -1,24 +1,31 @@
 <template>
   <div>
-    <h1 class="text-center">Vos Réservations</h1>
     <div class="customlist col-12">
-            <div class="item row border rounded border-alert-dark p-3 mt-4" v-for="booking in bookings" :key="booking.id">
-                <div class="col-md-2">{{booking.car.Name}}</div>
-                <div class="col-md-1">{{booking.car.PlaceNb}} places</div>
-                <div class="col-md-2">{{booking.car.Price/100}}€/heure</div>
-                <div class="col-md-2">{{booking.StartDate}}</div>
-                <div class="col-md-2">{{booking.EndDate}}</div>
-                <div class="col-md-1">{{booking.Total/100}}€</div>
-                <div class="col-md-2 text-center">{{booking.State}}</div>
-            </div>
-        </div>
-        <div class="col-md-2 text-center">{{test}}</div>
+      <div class="item row border rounded border-alert-dark p-3 mt-4">
+          <div class="col-md-2">Nom du véhicule</div>
+          <div class="col-md-2">Nombre de place</div>
+          <div class="col-md-2">Coût</div>
+          <div class="col-md-3">Durée de la réservation</div>
+          <div class="col-md-1">Total</div>
+      </div>
+      <div class="item row border rounded border-alert-dark p-3 mt-4" v-for="booking in bookings" :key="booking.id">
+          <div class="col-md-2">{{booking.Car[0].Name[0]}}</div>
+          <div class="col-md-2">{{booking.Car[0].PlaceNb[0]}} places</div>
+          <div class="col-md-2">{{booking.Car[0].Price[0]/100}}€/jour</div>
+          <div class="col-md-3">Du {{booking.StartDate[0]}} au {{booking.EndDate[0]}}</div><!-- TODO formater la date (enlever l'heure) -->
+          <div class="col-md-1">{{GetTotal(booking)}}€</div>
+          <div class="col-md-2 text-center">{{booking.State[0]}}</div>
+      </div>
+    </div>
+    <div class="col-md-2 text-center">{{test}}</div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
-const parseString = require('xml2js').parseString;
+axios.defaults.baseURL = 'http://192.168.43.240:52066/VroomService.asmx/'
+const parseString = require('xml2js').parseString
+const querystring = require('querystring')
 
 export default {
   name: 'Reservations',
@@ -28,55 +35,58 @@ export default {
       users: [],
       test: "",
       id :1,
-    
     }
   },
   methods: {
-    // On récupère le numéro d'identification de l'utilisateur.
-    getIdUser: function() {
-          var vm = this
-            axios.get('http://localhost:52066/VroomService.asmx/GetUserById').then(response => {
-                parseString(response.data, (err, result) => {
-                    vm.users = result.ArrayofUser.User
-                });
-            })
-            .catch(error => {
-                vm.test = error
+    // * On récupère le numéro d'identification de l'utilisateur.
+    getIdUser() {
+      var vm = this
+        axios.get('GetUserById').then(response => {
+            parseString(response.data, (err, result) => {
+                vm.users = result.ArrayofUser.User
             });
-        },/*
-        // On affiche la liste des réservations pour l'utilisateur connecté.
-        postBookings: function() {
-          // TODO : récupérer l'id de l'utilisateur.
-            var vm = this
-            axios.post('http://localhost:52066/VroomService.asmx/GetListBooking').then(response => {
-                parseString(response.data, (err, result) => {
-                    vm.bookings = result.ArrayOfBooking.Booking
-                });
-            })
-            .catch(error => {
-                vm.test = error
-            });
-        },*/
-        
-        cancelBookings: function() {
-          // TODO : récupérer l'id de l'utilisateur.
-            var vm = this
-            axios.post('http://localhost:52066/VroomService.asmx/GetListBooking', {user_id : vm.id}).then(response => {
-                parseString(response.data, (err, result) => {
-                    vm.bookings = result.ArrayOfBooking.Booking
-                    // vm.users = result.ArrayOfUsers.User
-                });
-            })
-            .catch(error => {
-                vm.test = error
-            });
-        }
+        })
+        .catch(error => {
+            vm.test = error
+        });
     },
-    mounted: function() {
-       // this.getIdUser(),
-       // this.postBookings(),
-        this.cancelBookings()
-    }
 
+    // * On affiche la liste des réservations pour l'utilisateur connecté.
+    postBookings() {
+      // TODO : récupérer l'id de l'utilisateur.
+        var vm = this
+        axios.post('GetListBooking', querystring.stringify({ user_id: '1'})).then(response => {
+            parseString(response.data, (err, result) => {
+                vm.bookings = result.ArrayOfBooking.Booking
+            });
+        })
+        .catch(error => {
+            vm.test = error
+        });
+    },
+
+    cancelBookings() {
+      // TODO : récupérer l'id de l'utilisateur.
+        var vm = this
+        axios.post('GetListBooking').then(response => {
+            parseString(response.data, (err, result) => {
+                vm.bookings = result.ArrayOfBooking.Booking
+                vm.test = result
+            });
+        })
+        .catch(error => {
+            vm.test = error
+        });
+    },
+
+    GetTotal(/*booking*/) {
+      // TODO retourner le total
+    }
+  },
+  mounted: function() {
+      // this.getIdUser(),
+    this.postBookings()
+      //this.cancelBookings()
+  }
 }
 </script>

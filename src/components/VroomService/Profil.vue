d4c13db66298bab1a21773de2acb841bdef82855
emiliego <e.gougeon@iia-laval.fr>
2019-12-11 09:49:16 +0100

    Add profil template and reservation post

--- src/components/VroomService/Profil.vue
+++ src/components/VroomService/Profil.vue
@@ -1,7 +1,43 @@
 <template>
     <div>
-        <p>Profil</p>
+        <h1 class="text-center">Profil</h1>
+<form>
+    <div class="col-12 rounded mt-2">
+  <div class="form-group row">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">Pseudonyme</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputEmail3" placeholder="Pseudonyme" >
     </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Nom</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputPassword3" placeholder="Nom">
+    </div>
+  </div>
+    <div class="form-group row">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Prénom</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputPassword3" placeholder="Prénom">
+    </div>
+  </div>
+    <div class="form-group row">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Mot de passe</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3" placeholder="Mot de passe">
+
+  </div>
+      </div>
+  </div>
+  <div class="form-group row">
+    <div class="col-sm-10">
+      <button type="submit" class="btn btn-primary">Enregistrer</button>
+    </div>
+  </div>
+ 
+</form>
+</div>
+
 </template>
 
 <script>
--- src/components/VroomService/Reservations.vue
+++ src/components/VroomService/Reservations.vue
@@ -3,50 +3,80 @@
     <h1 class="text-center">Vos Réservations</h1>
     <div class="customlist col-12">
             <div class="item row border rounded border-alert-dark p-3 mt-4" v-for="booking in bookings" :key="booking.id">
-                <div class="col-md-2">{{booking.car.name}}</div>
-                <div class="col-md-1">{{booking.car.nbplace}} places</div>
-                <div class="col-md-2">{{booking.car.price/100}}€/heure</div>
-                <div class="col-md-2">{{booking.start_date}}</div>
-                <div class="col-md-2">{{booking.end_date}}</div>
-                <div class="col-md-1">{{booking.total/100}}€</div>
-                <div class="col-md-2 text-center">{{booking.state}}</div>
+                <div class="col-md-2">{{booking.car.Name}}</div>
+                <div class="col-md-1">{{booking.car.PlaceNb}} places</div>
+                <div class="col-md-2">{{booking.car.Price/100}}€/heure</div>
+                <div class="col-md-2">{{booking.StartDate}}</div>
+                <div class="col-md-2">{{booking.EndDate}}</div>
+                <div class="col-md-1">{{booking.Total/100}}€</div>
+                <div class="col-md-2 text-center">{{booking.State}}</div>
             </div>
         </div>
+        <div class="col-md-2 text-center">{{test}}</div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+const parseString = require('xml2js').parseString;
+
 export default {
   name: 'Reservations',
   data: () => {
     return {
-      bookings: [
-        {
-          id: 0,
-          car: {
-            name: 'name car',
-            nbplace: 2,
-            price: 3500
-          },
-          start_date: '9/12/2019 9:00:00',
-          end_date: '9/12/2019 18:00:00',
-          total: 31500,
-          state: 'En cours'
-        },
-        {
-          id: 0,
-          car: {
-            name: 'name 2 car',
-            nbplace: 2,
-            price: 2700
-          },
-          start_date: '9/12/2019 10:00:00',
-          end_date: '9/12/2019 11:00:00',
-          total: 5400,
-          state: 'Terminée'
-        }
-      ]
+      bookings: [],
+      users: [],
+      test: "",
+      id :1,
+    
     }
-  }
+  },
+  methods: {
+    // On récupère le numéro d'identification de l'utilisateur.
+    getIdUser: function() {
+          var vm = this
+            axios.get('http://localhost:52066/VroomService.asmx/GetUserById').then(response => {
+                parseString(response.data, (err, result) => {
+                    vm.users = result.ArrayofUser.User
+                });
+            })
+            .catch(error => {
+                vm.test = error
+            });
+        },/*
+        // On affiche la liste des réservations pour l'utilisateur connecté.
+        postBookings: function() {
+          // TODO : récupérer l'id de l'utilisateur.
+            var vm = this
+            axios.post('http://localhost:52066/VroomService.asmx/GetListBooking').then(response => {
+                parseString(response.data, (err, result) => {
+                    vm.bookings = result.ArrayOfBooking.Booking
+                });
+            })
+            .catch(error => {
+                vm.test = error
+            });
+        },*/
+        
+        cancelBookings: function() {
+          // TODO : récupérer l'id de l'utilisateur.
+            var vm = this
+            axios.post('http://localhost:52066/VroomService.asmx/GetListBooking', {user_id : vm.id}).then(response => {
+                parseString(response.data, (err, result) => {
+                    vm.bookings = result.ArrayOfBooking.Booking
+                    // vm.users = result.ArrayOfUsers.User
+                });
+            })
+            .catch(error => {
+                vm.test = error
+            });
+        }
+    },
+    mounted: function() {
+       // this.getIdUser(),
+       // this.postBookings(),
+        this.cancelBookings()
+    }
+
 }
 </script>
